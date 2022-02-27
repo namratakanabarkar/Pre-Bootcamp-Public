4db79798c1f9352e4c05bb9b0b1b76b9941ee171
namratakanabarkar <99300625+namratakanabarkar@users.noreply.github.com>
2022-02-27 10:58:22 -0800

    Add files via upload
    Button Click Assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+function removeName(element) {
+    element.innerText = "";
+    console.log("This is the message from function.");
+}
+
+function change(element) {
+    element.innerText = "LogOut";
+    console.log("This is change function");
+}
